ebbbf808f9ea96ded5f7af1eba899f8caef66327
luccascleann <luccas@cleanndistribuidora.com>
2022-08-30 08:54:32 -0300

    Criação do Player E Unitest

---
+++ estruturado/src/player.py
@@ -0,0 +1,13 @@
+class Player:
+
+    def escolher_jogada(self, jogada: str) -> int:
+        if self.jogada_eh_valida(jogada):
+            return int(jogada)
+        
+        print('\nValor fora das opções!!!\n')
+        return
+    
+    @staticmethod
+    def jogada_eh_valida(jogada: str) -> bool:
+        jogadas_validas = ['1', '2', '3', '4']
+        return jogada in jogadas_validas
