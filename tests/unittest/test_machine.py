c276c1ca63054dc774793072529020b5977b1c10
luccascleann <luccas@cleanndistribuidora.com>
2022-08-31 10:46:47 -0300

    Fix MachineTest

--- tests/unittest/test_machine.py
+++ tests/unittest/test_machine.py
@@ -5,7 +5,7 @@ from src.machine import Machine
 @pytest.mark.unittest
 @pytest.mark.machine
 class MachineTests:
-    def test_player_escolhe_jogada_corretamente(self):
+    def test_machine_escolhe_jogada_corretamente(self):
         expected_results = [1, 2, 3]
         player = Machine()
         assert player.escolher_jogada() in expected_results
